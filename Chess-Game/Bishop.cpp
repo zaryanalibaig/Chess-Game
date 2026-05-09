95e4e6b616aa9327b0c751b857ecbe0d50ba7724
Ali Abdullah <f250055@cfd.nu.edu.pk>
2026-05-09 15:38:05 +0500

    Comments added

--- Chess-Game/Bishop.cpp
+++ Chess-Game/Bishop.cpp
@@ -4,11 +4,14 @@ Bishop::Bishop(Position pos, Color color) {
 	this->color = color;
 }
 bool Bishop::isValidmove(Position to, Board& B) {
+	// checking if the square is not empty and also the color is same then 
 	if (B.Grid[to.row][to.col] != nullptr && B.Grid[to.row][to.col]->color == this->color) {
 		return false;
 	}
+	// finding row difference and column difference for bishop movement 
 	int rowDif = pos.row - to.row;
 	int colDif = pos.col - to.col;
+	// row and column difference must be same as bishop moves in diagonal
 	if (abs(rowDif) != abs(colDif)) {
 		return false;
 	}
@@ -27,6 +30,7 @@ bool Bishop::isValidmove(Position to, Board& B) {
 	int r = pos.row + rowStep;
 	int c = pos.col + colStep;
 	while (r != to.row || c != to.col) {
+		// chcecking if a piece is blocking the path 
 		if (B.Grid[r][c] != nullptr) {
 			return false;
 		}
@@ -35,6 +39,7 @@ bool Bishop::isValidmove(Position to, Board& B) {
 	}
 	return true;
 }
+// getting symbol to diaply it in board displaying
 char Bishop::getSymbol() {
 	return 'B';
 }
